fb8ab5b35b8030e0c3c0d5e403dbf4be01e883e6
v5 <zhihao.ding@foxmail.com>
2023-04-04 14:07:19 +0800

    add frontend

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterView from '../views/RegisterView.vue';
+import LoginView from '../views/LoginView.vue';
+import DashboardView from '../views/DashboardView.vue';
+import ProfileView from '../views/ProfileView.vue';
+import NoteView from '../views/NoteView.vue';
+import EditNoteView from '../views/EditNoteView.vue';
+import store from '../store';
 
 const routes = [
   {
@@ -14,12 +21,60 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/note/:id',
+    name: 'Note',
+    component: NoteView,
+    meta: { requiresAuth: true },
+    props: true,
+  },
+  {
+    path: '/editnote/:id',
+    name: 'EditNote',
+    component: EditNoteView,
+    meta: { requiresAuth: true },
+    props: true,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, _, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isAuthenticated) {
+            next();
+            return;
+        }
+        next('/login');
+    } else {
+        next();
+    }
+});
 
 export default router
